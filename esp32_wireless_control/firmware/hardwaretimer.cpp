54e862e6c16ee95ac97a45e27bd1619ae07e876d
chemistorge <chemistorge@googlemail.com>
2024-11-03 18:17:00 +0000

    add files

---
+++ esp32_wireless_control/firmware/hardwaretimer.cpp
@@ -0,0 +1,29 @@
+#include "esp32-hal-timer.h"
+
+#include "hardwaretimer.h"
+
+
+HardwareTimer::HardwareTimer(uint64_t frequency) {
+  timer_pointer = timerBegin(frequency);
+}
+HardwareTimer::HardwareTimer(uint64_t frequency, void (*functionToCall)()) {
+  this->ISR_Function = functionToCall;
+  timer_pointer = timerBegin(frequency);
+  timerAttachInterrupt(timer_pointer, ISR_Function);
+}
+void HardwareTimer::attachInterupt(void (*functionToCall)()){
+  this->ISR_Function = functionToCall;
+  timerAttachInterrupt(timer_pointer, ISR_Function);
+}
+
+void HardwareTimer::start(uint64_t alarmValue, bool autoReload) {
+  timerAlarm(timer_pointer, alarmValue, true, 0);
+}
+
+void HardwareTimer::stop() {
+  timerStop(timer_pointer);
+}
+
+void HardwareTimer::setCountValue(uint64_t countValue) {
+  timerWrite(timer_pointer, countValue);
+}
---
+++ esp32_wireless_control/firmware/hardwaretimer.h
@@ -0,0 +1,25 @@
+#ifndef TIMER_H
+#define TIMER_H
+#include "esp32-hal-timer.h"
+
+
+class HardwareTimer {
+
+  private:
+
+  hw_timer_t* timer_pointer = NULL;
+  void (*ISR_Function)();
+
+  public:
+
+
+  HardwareTimer(uint64_t frequency);
+  HardwareTimer(uint64_t frequency, void (*functionToCall)());
+  void attachInterupt(void (*functionToCall)());
+  void start(uint64_t alarmValue, bool autoReload);
+  void stop();
+  void setCountValue(uint64_t countValue);
+};
+
+
+#endif
