aa3f747ba481d66b1d876c8a29ed2e33583287da
Sylensky <admin@medieval-realm.net>
2025-07-08 00:09:08 +0200

    firmware: change microstepping mode to 1/64 instead of 1/16
    Increase the smoothness of the steps during tracking to reduce
    jittering by increasing the microstepping mode.

--- esp32_wireless_control/firmware/tracking_rates.h
+++ esp32_wireless_control/firmware/tracking_rates.h
@@ -5,26 +5,26 @@
 
 #if MOTOR_TRACKING_RATE == TRACKING_RATE_BOARD_V2
 #if STEPPER_TYPE == STEPPER_0_9
-// gear ratio 101.25, 0.9deg motor, 16 msteps, f_cpu@80MHz
-#define TRACKER_MOTOR_MICROSTEPPING 16
+// gear ratio 101.25, 0.9deg motor, 64 msteps, f_cpu@240MHz, f_isr@40MHz
+#define TRACKER_MOTOR_MICROSTEPPING 64
 #define STEPS_PER_SECOND_256MICROSTEP 120
 
 enum trackingRateS
 {
-    TRACKING_SIDEREAL = 2659383, // SIDEREAL (23h,56 min)
-    TRACKING_SOLAR = 2666667,    // SOLAR (24h)
-    TRACKING_LUNAR = 2723867,    // LUNAR (24h, 31 min)
+    TRACKING_SIDEREAL = 664846, // SIDEREAL (23h,56 min)
+    TRACKING_SOLAR = 666667,    // SOLAR (24h)
+    TRACKING_LUNAR = 680967,    // LUNAR (24h, 31 min)
 };
 #else // stepper 1.8 deg
-// gear ratio 101.25, 1.8deg motor, 16 msteps, f_cpu@80MHz
-#define TRACKER_MOTOR_MICROSTEPPING 16
+// gear ratio 101.25, 1.8deg motor, 64 msteps, f_cpu@240MHz, f_isr@40MHz
+#define TRACKER_MOTOR_MICROSTEPPING 64
 #define STEPS_PER_SECOND_256MICROSTEP 60
 
 enum trackingRateS
 {
-    TRACKING_SIDEREAL = 5318765, // SIDEREAL (23h,56 min)
-    TRACKING_SOLAR = 5333333,    // SOLAR (24h)
-    TRACKING_LUNAR = 5447735,    // LUNAR (24h, 31 min)
+    TRACKING_SIDEREAL = 1329691, // SIDEREAL (23h,56 min)
+    TRACKING_SOLAR = 1333333,    // SOLAR (24h)
+    TRACKING_LUNAR = 1361934,    // LUNAR (24h, 31 min)
 };
 #endif
 #else
